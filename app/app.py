ef096ba9f1fe9fa51619b9d62f777ee4a10af85e
Lucas de Almeida <lucasalmeida@Lucass-MacBook.local>
2025-06-10 15:53:24 -0300

    upload 2

--- app/app.py
+++ app/app.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
 # curl -X POST http://localhost:5000/predict \
-    #  -H "Content-Type: application/json" \
-    #  -d '{"inputs": [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]]}'
+#      -H "Content-Type: application/json" \
+#      -d '{"inputs": [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]]}'
 
     # pip3 install autoai-libs==2.0.* xgboost==2.0.* scikit-learn==1.3.* numpy
 
